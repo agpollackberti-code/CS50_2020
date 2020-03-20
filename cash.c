643e678a3691b4b1ca484d4c18b9fee831a2e32d
agpollackberti-code <agpollackberti-code@users.noreply.github.com>
2020-03-20 16:00:55 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- cash.c
+++ cash.c
@@ -4,13 +4,13 @@
 
 int main(void)
 {
-    //get and shape user input
+    //get and shape user input from float to int
     float changeOwed;
     do
     {
         changeOwed = get_float("Change owed: ");
     }
-    while(changeOwed <= 0 || changeOwed > 99);    
+    while (changeOwed <= 0 || changeOwed > 99);    
     
     int intOwed = round(changeOwed * 100);
 
@@ -18,58 +18,49 @@ int main(void)
     int remainingAmount = intOwed; 
     int coinCounter = 0;
 
-    if(remainingAmount >=25)
-        {
+    //now start getting greedy!
+    //can I use quarters?
+    if (remainingAmount >= 25)
+    {
         do
         {
-            remainingAmount = (remainingAmount-25);
+            remainingAmount = (remainingAmount - 25);
             coinCounter++;
         }   
-        while  (remainingAmount >=25);
-        }
-
-    if(remainingAmount >=10)
-        {
-        do
-        {
-            remainingAmount = (remainingAmount-10);
-            coinCounter++;
-        }
-        while  (remainingAmount >=10);
-        }
-    
-    if(remainingAmount >= 5)
+        while (remainingAmount >= 25);
+    }
+    //can I use dimes?
+    if (remainingAmount >= 10)
     {
         do
         {
-            remainingAmount = (remainingAmount-5);
+            remainingAmount = (remainingAmount - 10);
             coinCounter++;
         }
-        while  (remainingAmount >=5);
+        while (remainingAmount >= 10);
     }
-
-    if(remainingAmount >=1 )
+    //can I use nickels?
+    if (remainingAmount >= 5)
     {
         do
         {
-            remainingAmount = (remainingAmount-1);
+            remainingAmount = (remainingAmount - 5);
             coinCounter++;
         }
-        while  (remainingAmount >=1);
+        while (remainingAmount >= 5);
+    }
+    //can I use pennies?
+    if (remainingAmount >= 1)
+    {
+        do
+        {
+            remainingAmount = (remainingAmount - 1);
+            coinCounter++;
+        }
+        while (remainingAmount >= 1);
     }
 
+    //once we've exhausted remainingAmount, then return the count
     printf("%i \n", coinCounter);
-
-    
-
-    
-    //is it greater than 25?  Then subtract 25 and increment counter up by 1
-    //is it less than 25, then do 10
-    //is it less then 10?, then do 5
-    //is it less than 5, then do 1 until it = 0
-    //return counter value 
-
-    //printf to output answer
-    //output should be total # of coins, in an int followed by \n
 
 }
