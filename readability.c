0197b27b407589012e3b6abcfa2569cd30a2090a
agpollackberti-code <agpollackberti-code@users.noreply.github.com>
2020-03-25 21:44:17 +0000

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -11,19 +11,21 @@ int main(void)
     //get user input in string format with get_string
     string s = get_string("Text: ");
 
+    //evaluate for the index
     int index = evaluation(s);
 
     //shape output
-
-    if(index > 16)
+    if (index > 16)
     {
         printf("Grade 16+\n");
     }
-    if(index < 1)
+
+    if (index < 1)
     {
         printf("Before Grade 1\n");
     }
-    if(index > 1 && index <=16)
+
+    if (index > 1 && index < = 16)
     {
         printf("Grade %i\n", index);
     }
@@ -59,12 +61,11 @@ int evaluation(string input)
 
     }
 
-    //feed variables into the Coleman-Liau index calculation, return X
-
+    //feed above variables into the Coleman-Liau index calculation, return X
     //L = average number of letters per 100 words
-    float L = ((letters/words)*100);
+    float L = ((letters / words) * 100);
     //S = average number of sentences per 100 words
-    float S = ((sentences/words)*100);
+    float S = ((sentences / words) * 100);
     //index = 0.0588 * L - 0.296 * S - 15.8
     int index = round(0.0588 * L - 0.296 * S - 15.8);
     return index;
