e6da8ce159030472accc3ff1fa04f810bf610e9b
agpollackberti-code <agpollackberti-code@users.noreply.github.com>
2020-03-26 01:29:02 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -67,13 +67,14 @@ int main(int argc, string argv[])
                 //if we exceed the range of 26 alphabet chars
                 //we need to know %26 when we exceed bounds, because we can use that remainder to "wrap" from a
                 //like if we end up
-                ciphertext[i] = toupper((ciphertext[i] + cipher) % 26);
+                ciphertext[i] = toupper(((ciphertext[i] - 'A' + cipher) % 26) + 'A');
+
             }
 
             //Is it lower? then convert and toLower.
             if(islower(ciphertext[i]))
             {
-                ciphertext[i] = tolower((ciphertext[i] + cipher) % 26);
+                ciphertext[i] = tolower(((ciphertext[i] -'a' + cipher) % 26) + 'a');
             }
 
         }
