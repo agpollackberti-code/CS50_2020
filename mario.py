fc11b0738c741a8681fcb65ef3443d02bcec61b4
agpollackberti-code <agpollackberti-code@users.noreply.github.com>
2020-07-10 22:22:53 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,17 +1,14 @@
 from cs50 import get_int
 
-##get input
-
-
-##check input
+# get & check input
 while True:
     height = get_int("When we say jump Mario we say how high? ")
-    if 1 <= height <=8:
+    if 1 <= height <= 8:
         break
 
-##print based on input
+# print based on input
 for i in range(height):
-    print(" "*(height-i-1),end="")
+    print(" "*(height-i-1), end="")
     for j in (range(i+1)):
-        print("#",end="")
+        print("#", end="")
     print()
